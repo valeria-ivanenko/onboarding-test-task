f594fbd47067c7cbb26b2387428fe199de7f4e1a
Valeriia Ivanenko <valeria.ivanenko@icloud.com>
2025-11-10 21:13:35 +0200

    Create Constants.swift

---
+++ OnboardingTestTaskApp/OnboardingTestTaskApp/Resources/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  OnboardingTestTaskApp
+//
+//  Created by Valeriia Ivanenko on 10.11.2025.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiURL = URL(string: "https://test-ios.universeapps.limited/onboarding")!
+}
